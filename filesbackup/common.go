1fb1a176abf01d2d60ed0d71cb195d740670093c
tigerinus <tigerwang@outlook.com>
2024-04-23 18:07:45 +0800

    add common funcs for `IceWhale-Files-Backup` module (#2)

---
+++ filesbackup/common.go
@@ -0,0 +1,64 @@
+package filesbackup
+
+import (
+	"encoding/json"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const ServiceName = "files-backup"
+
+func DefaultMetadataPath(dataPath string) string {
+	return filepath.Join(dataPath, ServiceName)
+}
+
+func GetAllBackups[M any](metadataPath string) (map[string][]M, error) {
+	// walk thru metadataPath and load each file starting with "backup_" in JSON format as a FolderBackup into a map, and return
+	// the map.
+
+	allBackups := map[string][]M{}
+
+	if err := filepath.WalkDir(metadataPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() || !strings.HasPrefix(d.Name(), "backup_") {
+			return nil
+		}
+
+		backup, err := LoadMetadata[M](path)
+		if err != nil {
+			return fs.SkipDir
+		}
+
+		// clientID is the directory name of path
+		clientID := filepath.Base(filepath.Dir(path))
+
+		allBackups[clientID] = append(allBackups[clientID], *backup)
+
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return allBackups, nil
+}
+
+func LoadMetadata[M any](metadataFilePath string) (*M, error) {
+	metadataFile, err := os.Open(metadataFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer metadataFile.Close()
+
+	decoder := json.NewDecoder(metadataFile)
+	var backup M
+	if err := decoder.Decode(&backup); err != nil {
+		return nil, err
+	}
+
+	return &backup, nil
+}
