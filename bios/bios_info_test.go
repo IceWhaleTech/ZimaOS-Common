b7edd8dca04761bd8e4fb7ae9af32779184ce0a5
Ns2Kracy <ns2kracy@gmail.com>
2024-07-02 03:23:19 +0000

    fix get model

---
+++ bios/bios_info_test.go
@@ -0,0 +1,22 @@
+package bios
+
+import "testing"
+
+func TestGetModel(t *testing.T) {
+	t.Run("ZimaCube", func(t *testing.T) {
+		// ccc
+		model := GetModel()
+		t.Log("Model:", model)
+		if model != ZIMACUBE {
+			t.Errorf("Expected %s, got %s", ZIMACUBE, model)
+		}
+	})
+	t.Run("ZimaCubePro", func(t *testing.T) {
+		// ccc
+		model := GetModel()
+		t.Log("Model:", model)
+		if model != ZIMACUBEPRO {
+			t.Errorf("Expected %s, got %s", ZIMACUBEPRO, model)
+		}
+	})
+}
